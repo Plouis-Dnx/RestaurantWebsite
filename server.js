77d1f8423dc6661b82e292fa38ba9ee394ffbc93
WildRock <plouis.deneux@gmail.com>
2025-04-01 16:55:27 +0200

    modifs

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./dependencies/app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}/accueil.html`);
 });
